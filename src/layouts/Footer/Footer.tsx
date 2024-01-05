27960d76df81221d626828abde15b985dc5b39fc
Oleksandr-Berd <alex.berd86@gmail.com>
2024-01-05 14:37:40 +0200

    all is done

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,9 +1,15 @@
+import { useMediaQuery } from 'usehooks-ts';
+
 import * as SC from './FooterStyled'
 
+import redWave from '../../assets/images/desktop/bg-pattern-wave-red.svg'
 import LinkBtn from "../../ui/LinkBtn/LinkBtn";
 
 const Footer:React.FC = () => {
-    return ( <SC.FooterStyled>
+
+const isDesk = useMediaQuery("(min-width:1440px)")
+
+    return ( <SC.FooterStyled bg={isDesk ? redWave : ""}>
         <SC.Content>Let's build something great together.</SC.Content>
         <LinkBtn path="#" content="Schedule a Call" position='footer'/>
     </SC.FooterStyled> );
--- src/layouts/Footer/FooterStyled.ts
+++ src/layouts/Footer/FooterStyled.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
+import { IStyles } from "../../utils/interfaces";
 
-export const FooterStyled = styled.footer`
+export const FooterStyled = styled.footer<Partial <IStyles>>`
   padding-top: 100px;
   padding-bottom: 100px;
   padding-left: 24px;
@@ -17,6 +18,24 @@ export const FooterStyled = styled.footer`
     padding-left: 39px;
     padding-right: 40px;
   }
+
+  @media (min-width: 1440px) {
+position: relative;
+
+    padding-top: 120px;
+    padding-bottom: 115px;
+    padding-left: 165px;
+    padding-right: 165px;
+
+    &::before{
+        content: url(${props => props.bg});
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        transform: translateX(-67px) translateY(154px);
+    }
+  }
 `;
 
 export const Content = styled.h3`
@@ -30,5 +49,12 @@ export const Content = styled.h3`
     width: 320px;
 
     margin-bottom: 0;
+  }
+
+  @media (min-width: 1440px) {
+    width: 635px;
+
+    font-size: 56px;
+    line-height: 1.14;
   }
 `;
